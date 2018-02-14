c5a26e634b072671370d50c755b3fd560b9cb5c7
wonheep <wonheeprk@gmail.com>
2018-02-13 19:33:37 -0800

    skeleton

---
+++ README.md
@@ -0,0 +1,35 @@
+# Command-line Blackjack Game Simulator
+
+*Author: Wonhee Park*
+
+**Files**
+* README.md
+* blackjack.py
+
+**Development Environment**
+* python 3
+
+**Running the Program**
+* ```python blackjack.py``` if you are on python3
+* if env is set to python2, install python3, and run ```python3 blackjack.py```
+
+## Design Choice
+
+
+## Implementation
+
+
+## Testing
+
+
+## Challenges
+
+
+## Key Information
+
+
+## Sources
+* Online Resources:
+	* game rules:[link1](http://www.blackjackinfo.com/blackjack-rules.php)
+
+
---
+++ src/blackjack.py
@@ -0,0 +1,36 @@
+# BLACKJACK GAME - SINGLE PLAYER
+# AUTHOR: Wonhee Park
+
+# LIBRARIES
+import random
+import sys
+
+# GLOBALS
+valid_input = ["h", "s", "H", "S"]
+num_dealings = 0
+num_games = 0
+num_cards = 52
+blackjack = 21
+
+dealer_cards = []
+player_cards = []
+
+# STRUCTS
+# CREATE card shuffling algorithm 
+
+# SUMMARY: deletes whitespace before, between and after text
+def delete_whitespace(player_input):
+
+# SUMMARY: error checking from user input
+def validate_input(player_input):
+
+# SUMMARY: shuffle what remains of the deck 
+def shuffle_cards(num_cards):
+
+def main():
+
+	while (1):
+		print("\nWelcome to a game of blackjack!\n")
+		
+if __name__ == "__main__":
+    main()
